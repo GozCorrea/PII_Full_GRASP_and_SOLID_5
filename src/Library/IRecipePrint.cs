d4f3a5976c4bbd9db534f42ca8884611160cd94e
GozCorrea <62217597+GozCorrea@users.noreply.github.com>
2020-10-29 21:56:34 -0300

    comentario

--- src/Library/IRecipePrint.cs
+++ src/Library/IRecipePrint.cs
@@ -1,6 +1,9 @@
 
 namespace Full_GRASP_And_SOLID
 {
+
+    //Se crea interface IRecipeprint para poder cumplir con el principio de inversion. Esta es la abstaccion
+    //que encontramos para poder aplicar este principio
     public interface IRecipeprint
     {
         string GetTextToPrint();
